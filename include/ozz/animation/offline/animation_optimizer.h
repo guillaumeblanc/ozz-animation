7464af17485cad8aaf10139f93c493502de751cb
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2016-02-16 23:43:40 +0100

    Updates documentation.

--- include/ozz/animation/offline/animation_optimizer.h
+++ include/ozz/animation/offline/animation_optimizer.h
@@ -67,9 +67,9 @@ class AnimationOptimizer {
   // Scale optimization tolerance, ie: the norm of the difference of two scales.
   float scale_tolerance;
 
-  // Hierarchical translation optimization tolerance, defined as the distance at
-  // the far end of the hierarchy. This allows to consider the effect of an
-  // optimization of a joint on all its children hierarchy.
+  // Hierarchical translation optimization tolerance, ie: the maximum error
+  // (distance) that an optimization on a joint is allowed to generate on its
+  // whole child hierarchy.
   float hierarchical_tolerance;
 };
 }  // offline
