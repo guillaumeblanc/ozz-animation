39d87d83a6479e181beb8d311603b5a2200944ba
guillaumeblanc <guillaumeblanc.sc@gmail.com>
2017-07-26 18:15:27 +0200

    Modifies Multithread sample to use c++11 std::async instead of OpenMP. This should make the sample work on more platforms.

--- samples/multithread/sample_multithread.cc
+++ samples/multithread/sample_multithread.cc
@@ -25,8 +25,8 @@
 //                                                                            //
 //----------------------------------------------------------------------------//
 
-#include <omp.h>
 #include <cstdlib>
+#include <future>
 
 #include "ozz/animation/runtime/animation.h"
 #include "ozz/animation/runtime/local_to_model_job.h"
@@ -69,51 +69,35 @@ const float kInterval = 2.f;
 const int kWidth = 16;
 const int kDepth = 16;
 
+// The maximum number of characters.
+const int kMaxCharacters = 4096;
+
+// Defines a TLS variable, which address is per thread and can thus be used to
+// identify a thread.
+thread_local size_t kTlsThreadIdentifier = 0;
+
 class MultithreadSampleApplication : public ozz::sample::Application {
  public:
   MultithreadSampleApplication()
-      : num_characters_(kWidth * kDepth),
-        enable_openmp_(true),
-        num_threads_(1),
-        openmp_statistics() {
-    // Do not allocate all threads to OpenMp by default, as it is too intensive.
-    const int max_threads = omp_get_max_threads();
-    num_threads_ = (max_threads > 2) ? max_threads - 1 : max_threads;
-  }
+      : characters_(kMaxCharacters),
+        thread_ids_(kMaxCharacters),
+        num_characters_(1024),
+        enable_theading_(true),
+        grain_size_(128) {}
 
  private:
   // Nested Character struct forward declaration.
   struct Character;
 
- protected:
-  // Updates current animation time.
-  virtual bool OnUpdate(float _dt) {
-#pragma omp parallel if (enable_openmp_) num_threads(num_threads_)
-    {
-// Collect open mp statistics on the master thread.
-#pragma omp single
-      {
-        openmp_statistics.num_procs = omp_get_num_procs();
-        openmp_statistics.num_threads = omp_get_num_threads();
-        openmp_statistics.max_threads = omp_get_max_threads();
-      }
-// Updates all animations.
-#pragma omp for
-      for (int i = 0; i < num_characters_; ++i) {
-        UpdateCharacter(&characters_[i], _dt);
-      }
-    }
-
-    return true;
-  }
-
-  bool UpdateCharacter(Character* _character, float _dt) {
+  static bool UpdateCharacter(const ozz::animation::Animation* _animation,
+                              const ozz::animation::Skeleton* _skeleton,
+                              float _dt, Character* _character) {
     // Samples animation.
-    _character->controller.Update(animation_, _dt);
+    _character->controller.Update(*_animation, _dt);
 
     // Setup sampling job.
     ozz::animation::SamplingJob sampling_job;
-    sampling_job.animation = &animation_;
+    sampling_job.animation = _animation;
     sampling_job.cache = _character->cache;
     sampling_job.time = _character->controller.time();
     sampling_job.output = _character->locals;
@@ -125,7 +109,7 @@ class MultithreadSampleApplication : public ozz::sample::Application {
 
     // Converts from local space to model space matrices.
     ozz::animation::LocalToModelJob ltm_job;
-    ltm_job.skeleton = &skeleton_;
+    ltm_job.skeleton = _skeleton;
     ltm_job.input = _character->locals;
     ltm_job.output = _character->models;
     if (!ltm_job.Run()) {
@@ -133,6 +117,47 @@ class MultithreadSampleApplication : public ozz::sample::Application {
     }
 
     return true;
+  }
+
+  static bool ParallelUpdate(int _grain_size,
+                             const ozz::animation::Animation* _animation,
+                             const ozz::animation::Skeleton* _skeleton,
+                             float _dt, Character* _characters, int _num,
+                             size_t** _thread_ids) {
+    bool success = true;
+    if (_num <= _grain_size) {
+      size_t* thread_id = &kTlsThreadIdentifier;
+      for (int i = 0; i < _num; ++i) {
+        _thread_ids[i] = thread_id;  // Store this thread identifier.
+        success &= UpdateCharacter(_animation, _skeleton, _dt, &_characters[i]);
+      }
+    } else {
+      const int half = _num / 2;
+      auto handle = std::async(std::launch::async, ParallelUpdate, _grain_size,
+                               _animation, _skeleton, _dt, _characters + half,
+                               _num - half, _thread_ids + half);
+      success &= ParallelUpdate(_grain_size, _animation, _skeleton, _dt,
+                                _characters, half, _thread_ids);
+      success &= handle.get();
+    }
+    return success;
+  }
+
+ protected:
+  // Updates current animation time.
+  virtual bool OnUpdate(float _dt) {
+    bool success = true;
+    if (enable_theading_) {
+      success = ParallelUpdate(grain_size_, &animation_, &skeleton_, _dt,
+                               array_begin(characters_), num_characters_,
+                               array_begin(thread_ids_));
+    } else {
+      for (int i = 0; i < num_characters_; ++i) {
+        success &= UpdateCharacter(&animation_, &skeleton_, _dt,
+                                   array_begin(characters_) + i);
+      }
+    }
+    return success;
   }
 
   // Renders all skeletons.
@@ -176,27 +201,39 @@ class MultithreadSampleApplication : public ozz::sample::Application {
     // Exposes multi-threading parameters.
     {
       static bool oc_open = true;
-      ozz::sample::ImGui::OpenClose oc(_im_gui, "OpenMP control", &oc_open);
+      ozz::sample::ImGui::OpenClose oc(_im_gui, "Threading control", &oc_open);
       if (oc_open) {
-        _im_gui->DoCheckBox("Enables OpenMP", &enable_openmp_);
-        char label[64];
-        std::sprintf(label, "Number of processors: %d",
-                     openmp_statistics.num_procs);
-        _im_gui->DoLabel(label);
+        _im_gui->DoCheckBox("Enables threading", &enable_theading_);
+        if (enable_theading_) {
+          char label[64];
+          std::sprintf(label, "Grain size: %d", grain_size_);
+          _im_gui->DoSlider(label, 64, kMaxCharacters, &grain_size_, .2f);
 
-        const int max = ozz::math::Max(2, openmp_statistics.max_threads + 2);
-        std::sprintf(label, "Number of threads: %d/%d", num_threads_, max);
-        _im_gui->DoSlider(label, 1, max, &num_threads_);
+          // Finds number of threads, which is the number of unique thread ids
+          // found.
+          std::sort(thread_ids_.begin(), thread_ids_.begin() + num_characters_);
+          auto end = std::unique(thread_ids_.begin(),
+                                 thread_ids_.begin() + num_characters_);
+          const int num_threads = static_cast<int>(end - thread_ids_.begin());
+          int num_tasks = 1;
+          for (int processed = grain_size_; processed < num_characters_;
+               processed *= 2, num_tasks *= 2)
+            ;
+          std::sprintf(label, "Thread/task count: %d/%d", num_threads,
+                       num_tasks);
+          _im_gui->DoLabel(label);
+        }
       }
     }
-    // Exposes sampling parameters.
+
+    // Exposes characters.
     {
       static bool oc_open = true;
       ozz::sample::ImGui::OpenClose oc(_im_gui, "Sample control", &oc_open);
       if (oc_open) {
         char label[64];
         std::sprintf(label, "Number of entities: %d", num_characters_);
-        _im_gui->DoSlider(label, 1, kMaxCharacters, &num_characters_, .5f);
+        _im_gui->DoSlider(label, 1, kMaxCharacters, &num_characters_, .7f);
         const int num_joints = num_characters_ * skeleton_.num_joints();
         std::sprintf(label, "Number of joints: %d", num_joints);
         _im_gui->DoLabel(label);
@@ -275,31 +312,23 @@ class MultithreadSampleApplication : public ozz::sample::Application {
     ozz::Range<ozz::math::Float4x4> models;
   };
 
-  // The maximum number of characters.
-  enum {
-    kMaxCharacters = 4096,
-  };
-
   // Array of characters of the sample.
-  Character characters_[kMaxCharacters];
+  ozz::Vector<Character>::Std characters_;
 
   // Number of used characters.
   int num_characters_;
 
-  // Enables/disables OpenMP.
-  bool enable_openmp_;
+  // Enable or disable threading.
+  bool enable_theading_;
 
-  // The number of threads as selected from the UI.
-  int num_threads_;
+  // Define the number of characters that a task can handle.
+  int grain_size_;
 
-  struct {
-    int num_procs;
-    int num_threads;
-    int max_threads;
-  } openmp_statistics;
+  // Array of thread identifiers used when updating characters.
+  ozz::Vector<size_t*>::Std thread_ids_;
 };
 
 int main(int _argc, const char** _argv) {
-  const char* title = "Ozz-animation sample: Multi-threading with OpenMp";
+  const char* title = "Ozz-animation sample: Multi-threading";
   return MultithreadSampleApplication().Run(_argc, _argv, "1.0", title);
 }
