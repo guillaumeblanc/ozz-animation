b3d0b4e9da03991cce0df30912738e64878a5b0b
guillaumeblanc <guillaumeblanc.sc@gmail.com>
2017-08-24 13:56:27 +0200

    Adds nlohmann::json as an extern library.

--- src/animation/offline/tools/convert2anim.cc
+++ src/animation/offline/tools/convert2anim.cc
@@ -30,6 +30,9 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <json.hpp>
+using json = nlohmann::json;
+
 #include "ozz/animation/offline/additive_animation_builder.h"
 #include "ozz/animation/offline/animation_builder.h"
 #include "ozz/animation/offline/animation_optimizer.h"
