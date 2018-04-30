648c728b6fcfafc9fbb9a4b8019858f5d6759b16
guillaumeblanc <guillaumeblanc.sc@gmail.com>
2018-04-30 13:31:34 +0200

    Improves documentation.

--- include/ozz/animation/offline/tools/convert2ozz.h
+++ include/ozz/animation/offline/tools/convert2ozz.h
@@ -41,12 +41,29 @@ class Skeleton;
 
 namespace offline {
 
+// Defines ozz converter/importer interface.
+// OzzConverter implements a command line tool to convert any source data format
+// to ozz skeletons and animations. The tool exposes a set of global options
+// through the command line, and a json configuration file to tune import
+// settings. Reference json configuration is generated at
+// src\animation\offline\tools\reference.json.
+// To import a new source data format, one will implement the pure virtual
+// functions of this interface. All the conversions end error processing are
+// done by the tool.
 class OzzConverter {
  public:
+  // Function operator that must be called with main() arguments to start import
+  // process.
   int operator()(int _argc, const char** _argv);
+
+ private:
+  // Loads source data file.
+  // Returning false will report and error.
+  virtual bool Load(const char* _filename) = 0;
 
   // Skeleton management.
 
+  // Defines node types that should be considered as skeleton joints.
   struct NodeType {
     bool skeleton : 1;  // Uses skeleton nodes as skeleton joints.
     bool marker : 1;    // Uses marker nodes as skeleton joints.
@@ -57,30 +74,43 @@ class OzzConverter {
                    // listed above and any other.
   };
 
+  // Import a skeleton from the source data file.
+  // Returning false will report and error.
   virtual bool Import(ozz::animation::offline::RawSkeleton* _skeleton,
                       const NodeType& _types) = 0;
 
   // Animations management.
 
+  // Gets the name of all the animations/clips/takes available from the source
+  // data file.
   typedef ozz::Vector<ozz::String::Std>::Std AnimationNames;
-
   virtual AnimationNames GetAnimationNames() = 0;
 
+  // Import animation "_animation_name" from the source data file.
+  // The skeleton is provided such that implementation can look for its joints
+  // animations.
+  // Returning false will report and error.
   virtual bool Import(const char* _animation_name,
                       const ozz::animation::Skeleton& _skeleton,
                       float _sampling_rate, RawAnimation* _animation) = 0;
 
   // Tracks / properties management.
 
+  // Defines properties, aka user-channel data: animations that aren't only
+  // joint transforms.
   struct NodeProperty {
     ozz::String::Std name;
     enum Type { kFloat1 = 1, kFloat2 = 2, kFloat3 = 3, kFloat4 = 4 };
     Type type;
   };
-  typedef ozz::Vector<NodeProperty>::Std NodeProperties;
 
+  // Get all properties available for a node.
+  typedef ozz::Vector<NodeProperty>::Std NodeProperties;
   virtual NodeProperties GetNodeProperties(const char* _node_name) = 0;
 
+  // Imports a track of type 1, 2, 3 or 4 floats, for the triplet
+  // _animation_name/_node_name/_track_name.
+  // Returning false will report and error.
   virtual bool Import(const char* _animation_name, const char* _node_name,
                       const char* _track_name, float _sampling_rate,
                       RawFloatTrack* _track) = 0;
@@ -93,9 +123,6 @@ class OzzConverter {
   virtual bool Import(const char* _animation_name, const char* _node_name,
                       const char* _track_name, float _sampling_rate,
                       RawFloat4Track* _track) = 0;
-
- private:
-  virtual bool Load(const char* _filename) = 0;
 };
 }  // namespace offline
 }  // namespace animation
