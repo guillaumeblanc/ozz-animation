bbfe239ddebe6b58f72e72f4a7ce14db29c91063
guillaumeblanc <guillaumeblanc.sc@gmail.com>
2018-11-27 13:53:53 +0100

    Cleanup sample options.

--- samples/two_bone_ik/sample_two_bone_ik.cc
+++ samples/two_bone_ik/sample_two_bone_ik.cc
@@ -33,10 +33,8 @@
 
 #include "ozz/base/maths/box.h"
 #include "ozz/base/maths/simd_math.h"
-#include "ozz/base/maths/soa_transform.h"
-
-// TODO Move
 #include "ozz/base/maths/simd_quaternion.h"
+#include "ozz/base/maths/soa_transform.h"
 
 #include "ozz/base/memory/allocator.h"
 
@@ -46,12 +44,6 @@
 #include "framework/imgui.h"
 #include "framework/renderer.h"
 #include "framework/utils.h"
-
-// TEMP
-#include "ozz/animation/offline/raw_skeleton.h"
-#include "ozz/animation/offline/skeleton_builder.h"
-
-ozz::math::SimdFloat4 g_target_pos;
 
 void ApplyQuaternion(int _joint, const ozz::math::SimdQuaternion& _quat,
                      const ozz::Range<ozz::math::SoaTransform>& _transforms) {
@@ -76,81 +68,65 @@ OZZ_OPTIONS_DECLARE_STRING(skeleton,
 class TwoBoneIKSampleApplication : public ozz::sample::Application {
  public:
   TwoBoneIKSampleApplication()
-      : skeleton_(NULL),
-        start_joint_(-1),
-        mid_joint_(-1),
-        end_joint_(-1),
-        doik(true),
+      : fix_initial_transform_(true),
         root_translation_(0.f, 0.f, 0.f),
         root_euler_(0.f, 0.f, 0.f),
         root_scale_(1.f),
+        target_time_(0.f),
+        target_extent_(.2f),
+        target_offset_(.3f, .1f, 0.f),
+        target_(0.f, 0.f, 0.f),
+        start_joint_(-1),
+        mid_joint_(-1),
+        end_joint_(-1),
         pole_vector(0.f, 1.f, 0.f),
         weight_(1.f),
-        soften_(1.f),
+        soften_(.97f),
         twist_angle_(0.f),
-        extent(0.f),
-        target_offset(1.f, .1f, 0.f),
-        use_loaded_skeleton_(true),
         show_target_(true),
         show_joints_(false),
         show_pole_vector_(false) {}
 
  protected:
   virtual bool OnUpdate(float _dt) {
-    (void)_dt;
+    // Updates sample target position.
+    MoveTarget(_dt);
 
-    // Reset locals to skeleton bind pose (for sample purpose).
-    for (size_t i = 0; i < locals_.size(); ++i) {
-      locals_[i] = skeleton_->joint_bind_poses()[i];
+    // Reset locals to skeleton bind pose if option is true.
+    // This allows to always start IK from a fix position (required to test
+    // weighting), or do IK from the latest computed pose.
+    if (fix_initial_transform_) {
+      for (size_t i = 0; i < locals_.size(); ++i) {
+        locals_[i] = skeleton_.joint_bind_poses()[i];
+      }
+    } else {
+      _dt = 0.f;
     }
-
-    // Build root matrix
-    using ozz::math::Float4x4;
 
     // Converts from local space to model space matrices.
     ozz::animation::LocalToModelJob ltm_job;
-    ltm_job.skeleton = skeleton_;
-    ltm_job.from = ozz::animation::Skeleton::kNoParent;  // TODO explain
-    ltm_job.to = end_joint_;
+    ltm_job.skeleton = &skeleton_;
     ltm_job.input = make_range(locals_);
     ltm_job.output = make_range(models_);
+
+    // Model-space matrics needs to be updated up to the end joint. Any joint
+    // after that will need to be recomputed after IK indeed.
+    ltm_job.from = ozz::animation::Skeleton::kNoParent;
+    ltm_job.to = end_joint_;
     if (!ltm_job.Run()) {
       return false;
     }
 
-    static float time = 0.f;
-    time += _dt;
-    g_target_pos = ozz::math::simd_float4::Load(
-        target_offset.x + extent * sinf(time),
-        target_offset.y + extent * cosf(time * 2.f),
-        target_offset.z + extent * cosf(time / 2.f), 0);
-
-    if (!IK()) {
-      return false;
-    }
-
-    ltm_job.from = start_joint_;                        // TODO explain
-    ltm_job.to = ozz::animation::Skeleton::kMaxJoints;  // end_joint_;
-    if (!ltm_job.Run()) {
-      return false;
-    }
-
-    return true;
-  }
-
-  bool IK() {
-    // Get root matrix, which is used at render time to convert object from
-    // model-space to world space.
-    const ozz::math::Float4x4 root = ComputeRootTransform();
-
-    // Target should be in model-space, so it must be converted from
+    // Target and pole should be in model-space, so they must be converted from
     // wolrd-space.
+    const ozz::math::Float4x4 root = GetRootTransform();
     const ozz::math::Float4x4 invert_root = Invert(root);
-    const ozz::math::SimdFloat4 target_ls =
-        TransformPoint(invert_root, g_target_pos);
+    const ozz::math::SimdFloat4 target_ls = TransformPoint(
+        invert_root, ozz::math::simd_float4::Load3PtrU(&target_.x));
     const ozz::math::SimdFloat4 pole_vector_ls = TransformVector(
         invert_root, ozz::math::simd_float4::Load3PtrU(&pole_vector.x));
 
+    // Setup and run IK job.
     ozz::animation::IKTwoBoneJob ik_job;
     ik_job.target = target_ls;
     ik_job.pole_vector = pole_vector_ls;
@@ -169,13 +145,16 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
       return false;
     }
 
-    if (doik) {
-      ApplyQuaternion(mid_joint_, mid_correction, make_range(locals_));
-      ApplyQuaternion(start_joint_, start_correction, make_range(locals_));
-    } else {
-      for (size_t i = 0; i < locals_.size(); ++i) {
-        locals_[i] = skeleton_->joint_bind_poses()[i];
-      }
+    // Apply IK quaternions to their respective local-space transforms.
+    ApplyQuaternion(mid_joint_, mid_correction, make_range(locals_));
+    ApplyQuaternion(start_joint_, start_correction, make_range(locals_));
+
+    // Updates model-space matrices no IK has been applied. All the ancestorsof
+    // the start of the IK chain must be computed.
+    ltm_job.from = start_joint_;
+    ltm_job.to = ozz::animation::Skeleton::kMaxJoints;
+    if (!ltm_job.Run()) {
+      return false;
     }
 
     return true;
@@ -185,21 +164,20 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
   virtual bool OnDisplay(ozz::sample::Renderer* _renderer) {
     bool success = true;
 
-    const float kBoxHalfSize = .005f;
-    const ozz::math::Float4x4 kAxesScale =
-        ozz::math::Float4x4::Scaling(ozz::math::simd_float4::Load1(.1f));
-
     // Get skeleton root transform.
-    const ozz::math::Float4x4 root = ComputeRootTransform();
+    const ozz::math::Float4x4 root = GetRootTransform();
 
     // Displays target
     if (show_target_) {
+      const float kBoxHalfSize = .005f;
       const ozz::sample::Renderer::Color colors[2] = {{0xff, 0xff, 0xff, 0xff},
                                                       {0xff, 0, 0, 0xff}};
       success &= _renderer->DrawBoxIm(
           ozz::math::Box(ozz::math::Float3(-kBoxHalfSize),
                          ozz::math::Float3(kBoxHalfSize)),
-          ozz::math::Float4x4::Translation(g_target_pos), colors);
+          ozz::math::Float4x4::Translation(
+              ozz::math::simd_float4::Load3PtrU(&target_.x)),
+          colors);
     }
 
     // Displays pole vector
@@ -216,59 +194,38 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
 
     // Showing joints
     if (show_joints_) {
+      const float kAxeScale = .1f;
+      const ozz::math::Float4x4 kAxesScale = ozz::math::Float4x4::Scaling(
+          ozz::math::simd_float4::Load1(kAxeScale));
       for (size_t i = 0; i < 3; ++i) {
-        const ozz::sample::Renderer::Color colors[6] = {
-            {0xff, 0, 0, 0xff}, {0xff, 0xff, 0xff, 0xff},
-            {0, 0xff, 0, 0xff}, {0xff, 0xff, 0xff, 0xff},
-            {0, 0, 0xff, 0xff}, {0xff, 0xff, 0xff, 0xff}};
         const int joints[3] = {start_joint_, mid_joint_, end_joint_};
         const ozz::math::Float4x4& transform = root * models_[joints[i]];
-        success &= _renderer->DrawBoxIm(
-            ozz::math::Box(ozz::math::Float3(-kBoxHalfSize),
-                           ozz::math::Float3(kBoxHalfSize)),
-            transform, colors + i * 2);
         success &= _renderer->DrawAxes(transform * kAxesScale);
       }
     }
 
     // Draws the animated skeleton posture.
-    success &= _renderer->DrawPosture(*skeleton_, make_range(models_), root);
+    success &= _renderer->DrawPosture(skeleton_, make_range(models_), root);
 
     return success;
   }
 
-  bool BuildSkeleton() {
-    // Delete previous skeleton
-    ozz::memory::default_allocator()->Delete(skeleton_);
-    skeleton_ = NULL;
-
-    // Generates or loads skeleton
-    if (use_loaded_skeleton_) {
-      ozz::memory::Allocator* allocator = ozz::memory::default_allocator();
-      skeleton_ = allocator->New<ozz::animation::Skeleton>();
-      if (!skeleton_ ||
-          !ozz::sample::LoadSkeleton(OPTIONS_skeleton, skeleton_)) {
-        return false;
-      }
-    } else {
-      // Generates runtime skeleton from editable raw skeleton
-      ozz::animation::offline::SkeletonBuilder builder;
-      skeleton_ = builder(editable_skeleton_);
-      if (!skeleton_) {
-        return false;
-      }
+  virtual bool OnInitialize() {
+    // Loads skeleton.
+    if (!ozz::sample::LoadSkeleton(OPTIONS_skeleton, &skeleton_)) {
+      return false;
     }
 
     // Allocates runtime buffers.
-    const int num_soa_joints = skeleton_->num_soa_joints();
+    const int num_soa_joints = skeleton_.num_soa_joints();
     locals_.resize(num_soa_joints);
-    const int num_joints = skeleton_->num_joints();
+    const int num_joints = skeleton_.num_joints();
     models_.resize(num_joints);
 
     // Find the 3 joints in skeleton hierarchy.
     start_joint_ = mid_joint_ = end_joint_ = -1;
-    for (int i = 0; i < skeleton_->num_joints(); i++) {
-      const char* joint_name = skeleton_->joint_names()[i];
+    for (int i = 0; i < skeleton_.num_joints(); i++) {
+      const char* joint_name = skeleton_.joint_names()[i];
       if (std::strcmp(joint_name, "shoulder") == 0) {
         start_joint_ = i;
       } else if (std::strcmp(joint_name, "forearm") == 0) {
@@ -285,42 +242,32 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
 
     // Initialize locals from skeleton bind pose
     for (size_t i = 0; i < locals_.size(); ++i) {
-      locals_[i] = skeleton_->joint_bind_poses()[i];
+      locals_[i] = skeleton_.joint_bind_poses()[i];
     }
     return true;
   }
 
-  virtual bool OnInitialize() {
-    // Prepares editable skeleton once for all
-    SetupEditableSkeleton();
-
-    // Builds current skeleton.
-    return BuildSkeleton();
-  }
-
-  virtual void OnDestroy() {
-    ozz::memory::Allocator* allocator = ozz::memory::default_allocator();
-    allocator->Delete(skeleton_);
-  }
+  virtual void OnDestroy() {}
 
   virtual bool OnGui(ozz::sample::ImGui* _im_gui) {
     char txt[32];
 
-    _im_gui->DoSlider("extent", 0.f, 5.f, &extent);
+    _im_gui->DoSlider("extent", -1.f, 1.f, &target_extent_);
 
     // IK parameters
+    _im_gui->DoCheckBox("Fix initial transform", &fix_initial_transform_);
 
-    {  // Handle
+    {  // Target position
       static bool opened = true;
-      ozz::sample::ImGui::OpenClose oc_pole(_im_gui, "Handle position",
+      ozz::sample::ImGui::OpenClose oc_pole(_im_gui, "Target position",
                                             &opened);
       const float kOffsetRange = 2.f;
-      sprintf(txt, "x %.2g", target_offset.x);
-      _im_gui->DoSlider(txt, -kOffsetRange, kOffsetRange, &target_offset.x);
-      sprintf(txt, "y %.2g", target_offset.y);
-      _im_gui->DoSlider(txt, -kOffsetRange, kOffsetRange, &target_offset.y);
-      sprintf(txt, "z %.2g", target_offset.z);
-      _im_gui->DoSlider(txt, -kOffsetRange, kOffsetRange, &target_offset.z);
+      sprintf(txt, "x %.2g", target_offset_.x);
+      _im_gui->DoSlider(txt, -kOffsetRange, kOffsetRange, &target_offset_.x);
+      sprintf(txt, "y %.2g", target_offset_.y);
+      _im_gui->DoSlider(txt, -kOffsetRange, kOffsetRange, &target_offset_.y);
+      sprintf(txt, "z %.2g", target_offset_.z);
+      _im_gui->DoSlider(txt, -kOffsetRange, kOffsetRange, &target_offset_.z);
     }
 
     {
@@ -384,56 +331,7 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
       _im_gui->DoCheckBox("Show pole vector", &show_pole_vector_);
     }
 
-    {  // Skeleton load/generate options
-      if (_im_gui->DoCheckBox("Use loaded skeleton", &use_loaded_skeleton_)) {
-        if (!BuildSkeleton()) {
-          return false;
-        }
-      }
-      if (!use_loaded_skeleton_) {
-        static bool opened = false;
-        ozz::sample::ImGui::OpenClose oc(_im_gui, "Edit skeleton joints",
-                                         &opened);
-        if (opened) {
-          if (raw_skeleton_editor.OnGui(&editable_skeleton_, _im_gui)) {
-            if (!BuildSkeleton()) {
-              return false;
-            }
-          }
-        }
-      }
-    }
     return true;
-  }
-
-  // Builds a simple skeleton that can be used to experiment different setups.
-  void SetupEditableSkeleton() {
-    const float kDefaultBoneLength = .3f;  // length
-
-    editable_skeleton_.roots.resize(1);
-    ozz::animation::offline::RawSkeleton::Joint& shoulder =
-        editable_skeleton_.roots[0];
-    shoulder.name = "shoulder";
-    shoulder.transform.translation = ozz::math::Float3::zero();
-    shoulder.transform.rotation = ozz::math::Quaternion::identity();
-    shoulder.transform.scale = ozz::math::Float3::one();
-
-    shoulder.children.resize(1);
-    ozz::animation::offline::RawSkeleton::Joint& forearm = shoulder.children[0];
-    forearm.name = "forearm";
-    forearm.transform.translation =
-        ozz::math::Float3::y_axis() * kDefaultBoneLength;
-    forearm.transform.rotation = ozz::math::Quaternion::FromAxisAngle(
-        ozz::math::Float3::z_axis(), ozz::math::kPi_2);
-    forearm.transform.scale = ozz::math::Float3::one();
-
-    forearm.children.resize(1);
-    ozz::animation::offline::RawSkeleton::Joint& wrist = forearm.children[0];
-    wrist.name = "wrist";
-    wrist.transform.translation =
-        ozz::math::Float3::y_axis() * kDefaultBoneLength;
-    wrist.transform.rotation = ozz::math::Quaternion::identity();
-    wrist.transform.scale = ozz::math::Float3::one();
   }
 
   virtual void GetSceneBounds(ozz::math::Box* _bound) const {
@@ -441,13 +339,23 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
 
     // Computes skeleton bound
     ozz::sample::ComputePostureBounds(make_range(models_), &posture_bound);
-    posture_bound = TransformBox(ComputeRootTransform(), posture_bound);
+    const ozz::math::Box posture_bound_ws =
+        TransformBox(GetRootTransform(), posture_bound);
 
     // Adds target in the bounds
-    *_bound = Merge(posture_bound, ozz::math::Box(target_offset));
+    *_bound = Merge(posture_bound_ws, ozz::math::Box(target_offset_));
   }
 
-  ozz::math::Float4x4 ComputeRootTransform() const {
+ private:
+  void MoveTarget(float _dt) {
+    target_time_ += _dt;
+    target_ = ozz::math::Float3(
+        target_offset_.x + target_extent_ * sinf(target_time_),
+        target_offset_.y + target_extent_ * cosf(target_time_ * 2.f),
+        target_offset_.z + target_extent_ * cosf(target_time_ / 2.f));
+  }
+
+  ozz::math::Float4x4 GetRootTransform() const {
     return ozz::math::Float4x4::Translation(
                ozz::math::simd_float4::Load3PtrU(&root_translation_.x)) *
            ozz::math::Float4x4::FromEuler(
@@ -456,9 +364,8 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
                ozz::math::simd_float4::Load1(root_scale_));
   }
 
- private:
   // Runtime skeleton.
-  ozz::animation::Skeleton* skeleton_;
+  ozz::animation::Skeleton skeleton_;
 
   // Buffer of local transforms as sampled from animation_.
   ozz::Vector<ozz::math::SoaTransform>::Std locals_;
@@ -466,19 +373,25 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
   // Buffer of model space matrices.
   ozz::Vector<ozz::math::Float4x4>::Std models_;
 
-  // Indices of the joints in the chain.
-  int start_joint_;
-  int mid_joint_;
-  int end_joint_;
-
-  bool doik;
+  bool fix_initial_transform_;
 
   // Root transformation.
   ozz::math::Float3 root_translation_;
   ozz::math::Float3 root_euler_;
   float root_scale_;
 
+  // Target positioning.
+  float target_time_;
+  float target_extent_;
+  ozz::math::Float3 target_offset_;
+  ozz::math::Float3 target_;
+
   // IK parameters.
+
+  // Indices of the joints in the chain.
+  int start_joint_;
+  int mid_joint_;
+  int end_joint_;
 
   // TODO
   ozz::math::Float3 pole_vector;
@@ -487,19 +400,6 @@ class TwoBoneIKSampleApplication : public ozz::sample::Application {
   float weight_;
   float soften_;
   float twist_angle_;
-
-  // Handle positioning.
-  float extent;
-  ozz::math::Float3 target_offset;
-
-  // Sample skeleton mode option.
-
-  // Uses loaded skeleton, if false then use editable skeleton below.
-  bool use_loaded_skeleton_;
-
-  // Raw skeleton that can be edited in UI
-  ozz::animation::offline::RawSkeleton editable_skeleton_;
-  ozz::sample::RawSkeletonEditor raw_skeleton_editor;
 
   // Sample display options
   bool show_target_;
