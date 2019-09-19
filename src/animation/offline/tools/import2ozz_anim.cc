7c0d5cd977ed8c3ef1d3110f5a10b94063a5483e
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2019-09-19 22:58:40 +0200

    Makes logs consistent.

--- src/animation/offline/tools/import2ozz_anim.cc
+++ src/animation/offline/tools/import2ozz_anim.cc
@@ -293,7 +293,8 @@ bool Export(OzzImporter& _importer, const RawAnimation& _input_animation,
     ozz::String::Std filename = _importer.BuildFilename(
         _config["filename"].asCString(), raw_animation.name.c_str());
 
-    ozz::log::LogV() << "Opens output file: " << filename << std::endl;
+    ozz::log::LogV() << "Opens output file: \"" << filename << "\""
+                     << std::endl;
     ozz::io::File file(filename.c_str(), "wb");
     if (!file.opened()) {
       ozz::log::Err() << "Failed to open output file: \"" << filename << "\""
@@ -309,7 +310,7 @@ bool Export(OzzImporter& _importer, const RawAnimation& _input_animation,
       ozz::log::Log() << "Outputs RawAnimation to binary archive." << std::endl;
       archive << raw_animation;
     } else {
-      ozz::log::LogV() << "Outputs Animation to binary archive." << std::endl;
+      ozz::log::Log() << "Outputs Animation to binary archive." << std::endl;
       archive << *animation;
     }
   }
