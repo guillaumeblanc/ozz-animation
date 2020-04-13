ca79ee9654375889b00f085b1dae8faff6dfa8b6
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2020-04-13 22:05:21 +0200

    Tests keyframe injection.

--- test/animation/offline/animation_builder_tests.cc
+++ test/animation/offline/animation_builder_tests.cc
@@ -25,19 +25,15 @@
 //                                                                            //
 //----------------------------------------------------------------------------//
 
-#include "ozz/animation/offline/animation_builder.h"
-
 #include "gtest/gtest.h"
-#include "ozz/base/maths/gtest_math_helper.h"
-
-#include "ozz/base/maths/soa_transform.h"
-#include "ozz/base/memory/unique_ptr.h"
-
+#include "ozz/animation/offline/animation_builder.h"
 #include "ozz/animation/offline/raw_animation.h"
-
 #include "ozz/animation/runtime/animation.h"
 #include "ozz/animation/runtime/sampling_job.h"
 #include "ozz/animation/runtime/skeleton.h"
+#include "ozz/base/maths/gtest_math_helper.h"
+#include "ozz/base/maths/soa_transform.h"
+#include "ozz/base/memory/unique_ptr.h"
 
 using ozz::animation::Animation;
 using ozz::animation::offline::AnimationBuilder;
@@ -344,4 +340,75 @@ TEST(Sort, AnimationBuilder) {
                               0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f);
     }
   }
+}
+
+TEST(ManyKeys, SamplingJob) {
+  const size_t kMaxKey = 300000;
+
+  RawAnimation raw_animation;
+  raw_animation.duration = 1.f;
+  raw_animation.tracks.resize(4);  // Adds a joint
+
+  // Track 0 has big hole
+  {
+    const RawAnimation::TranslationKey key0 = {
+        0.f, ozz::math::Float3(0.f, 0.f, 0.f)};
+    raw_animation.tracks[0].translations.push_back(key0);
+    const RawAnimation::TranslationKey key1 = {
+        .001f, ozz::math::Float3(10.f, 10.f, 10.f)};
+    raw_animation.tracks[0].translations.push_back(key1);
+    const RawAnimation::TranslationKey key2 = {
+        .98f, ozz::math::Float3(20.f, 20.f, 20.f)};
+    raw_animation.tracks[0].translations.push_back(key2);
+  }
+
+  // Track 1 has no key
+
+  // Track 2 has lots of keys
+  {
+    for (size_t i = 0; i < kMaxKey; ++i) {
+      const RawAnimation::TranslationKey key = {
+          i * 1.f / kMaxKey, ozz::math::Float3(std::cos(.5f * i))};
+      raw_animation.tracks[2].translations.push_back(key);
+    }
+  }
+
+  // Track 3 has big hole
+  {
+    const RawAnimation::TranslationKey key0 = {
+        0.f, ozz::math::Float3(0.f, 0.f, 0.f)};
+    raw_animation.tracks[3].translations.push_back(key0);
+    const RawAnimation::TranslationKey key1 = {
+        0.001f, ozz::math::Float3(1.f, 1.f, 1.f)};
+    raw_animation.tracks[3].translations.push_back(key1);
+    const RawAnimation::TranslationKey key2 = {
+        0.9f, ozz::math::Float3(2.f, 2.f, 2.f)};
+    raw_animation.tracks[3].translations.push_back(key2);
+    const RawAnimation::TranslationKey key3 = {
+        0.91f, ozz::math::Float3(3.f, 3.f, 3.f)};
+    raw_animation.tracks[3].translations.push_back(key3);
+  }
+
+  AnimationBuilder builder;
+  ozz::unique_ptr<Animation> animation = builder(raw_animation);
+  ASSERT_TRUE(animation);
+
+  ozz::animation::SamplingJob job;
+  job.animation = animation.get();
+  ozz::animation::SamplingCache cache(4);
+  job.cache = &cache;
+  job.ratio = 0.f;
+  ozz::math::SoaTransform output[1];
+  job.output = output;
+
+  EXPECT_TRUE(job.Validate());
+  EXPECT_TRUE(job.Run());
+  /*
+  EXPECT_SOAFLOAT3_EQ_EST(output[0].translation, 1.f, 0.f, 0.f, 0.f, -1.f, 0.f,
+                          0.f, 0.f, 5.f, 0.f, 0.f, 0.f);
+  EXPECT_SOAQUATERNION_EQ_EST(output[0].rotation, 0.f, 0.f, 0.f, 0.f, 0.f, 0.f,
+                              0.f, 0.f, 0.f, 0.f, 0.f, 0.f, 1.f, 1.f, 1.f, 1.f);
+  EXPECT_SOAFLOAT3_EQ_EST(output[0].scale, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f,
+                          1.f, 1.f, 1.f, 1.f, 1.f);
+                          */
 }
