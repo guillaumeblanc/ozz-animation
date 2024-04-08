11acb37513b2d17ca46ea3640c9eeaa2a450ac2e
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2024-04-08 21:20:11 +0200

    Optimizes cases that do not require a search through keyframes.

--- src/animation/runtime/track_sampling_job.cc
+++ src/animation/runtime/track_sampling_job.cc
@@ -51,9 +51,6 @@ bool TrackSamplingJob<_Track>::Run() const {
     return false;
   }
 
-  // Clamps ratio in range [0,1].
-  const float clamped_ratio = math::Clamp(0.f, ratio, 1.f);
-
   // Search keyframes to interpolate.
   const span<const float> ratios = track->ratios();
   const span<const ValueType> values = track->values();
@@ -66,27 +63,33 @@ bool TrackSamplingJob<_Track>::Run() const {
     return true;
   }
 
-  // Search for the first key frame with a ratio value greater than input ratio.
-  // Our ratio is between this one and the previous one.
-  const float* ptk1 =
-      std::upper_bound(ratios.begin(), ratios.end(), clamped_ratio);
-
-  // Deduce keys indices.
-  const size_t id1 = ptk1 - ratios.begin();
-  const size_t id0 = id1 - 1;
-
-  const bool id0step = (track->steps()[id0 / 8] & (1 << (id0 & 7))) != 0;
-  if (id0step || ptk1 == ratios.end()) {
-    *result = values[id0];
+  // Handles cases that do not require a search.
+  if (ratio <= 0.f) {
+    *result = values.data()[0];
+  } else if (ratio >= 1.f) {
+    *result = values.data()[values.size() - 1];
   } else {
-    // Lerp relevant keys.
-    const float tk0 = ratios[id0];
-    const float tk1 = ratios[id1];
-    assert(clamped_ratio >= tk0 && clamped_ratio < tk1 && tk0 != tk1);
-    const float alpha = (clamped_ratio - tk0) / (tk1 - tk0);
-    const ValueType& vk0 = values[id0];
-    const ValueType& vk1 = values[id1];
-    *result = internal::TrackPolicy<ValueType>::Lerp(vk0, vk1, alpha);
+    // Search for the first key frame with a ratio value greater than input
+    // ratio. Our ratio is between this one and the previous one.
+    const float* ptk1 = std::upper_bound(ratios.begin(), ratios.end(), ratio);
+
+    // Deduce keys indices.
+    const size_t id1 = ptk1 - ratios.begin();
+    const size_t id0 = id1 - 1;
+
+    const bool id0step = (track->steps()[id0 / 8] & (1 << (id0 & 7))) != 0;
+    if (id0step || ptk1 == ratios.end()) {
+      *result = values[id0];
+    } else {
+      // Lerp relevant keys.
+      const float tk0 = ratios[id0];
+      const float tk1 = ratios[id1];
+      assert(ratio >= tk0 && ratio < tk1 && tk0 != tk1);
+      const float alpha = (ratio - tk0) / (tk1 - tk0);
+      const ValueType& vk0 = values[id0];
+      const ValueType& vk1 = values[id1];
+      *result = internal::TrackPolicy<ValueType>::Lerp(vk0, vk1, alpha);
+    }
   }
   return true;
 }
