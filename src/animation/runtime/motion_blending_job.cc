787c11aea9281dee23a6983fc4dae27f712e89eb
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2024-04-29 22:27:30 +0200

    Implements motion blending job.

---
+++ src/animation/runtime/motion_blending_job.cc
@@ -0,0 +1,98 @@
+//----------------------------------------------------------------------------//
+//                                                                            //
+// ozz-animation is hosted at http://github.com/guillaumeblanc/ozz-animation  //
+// and distributed under the MIT License (MIT).                               //
+//                                                                            //
+// Copyright (c) Guillaume Blanc                                              //
+//                                                                            //
+// Permission is hereby granted, free of charge, to any person obtaining a    //
+// copy of this software and associated documentation files (the "Software"), //
+// to deal in the Software without restriction, including without limitation  //
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,   //
+// and/or sell copies of the Software, and to permit persons to whom the      //
+// Software is furnished to do so, subject to the following conditions:       //
+//                                                                            //
+// The above copyright notice and this permission notice shall be included in //
+// all copies or substantial portions of the Software.                        //
+//                                                                            //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    //
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING    //
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER        //
+// DEALINGS IN THE SOFTWARE.                                                  //
+//                                                                            //
+//----------------------------------------------------------------------------//
+
+#include "ozz/animation/runtime/motion_blending_job.h"
+
+#include "ozz/base/maths/transform.h"
+
+namespace ozz {
+namespace animation {
+
+bool MotionBlendingJob::Validate() const {
+  bool valid = true;
+
+  valid &= output != nullptr;
+
+  // Validates layers.
+  for (const Layer& layer : layers) {
+    valid &= layer.transform != nullptr;
+  }
+
+  return valid;
+}
+
+bool MotionBlendingJob::Run() const {
+  if (!Validate()) {
+    return false;
+  }
+
+  // Lerp each layer's transform according to its weight.
+  float acc_weight = 0.f;  // Accumulates weights for normalization.
+  float dl = 0.f;          // Weighted translation lengths.
+  ozz::math::Float3 dt = {0.f, 0.f, 0.f};  // Weighted translations directions.
+  ozz::math::Quaternion dr = {0.f, 0.f, 0.f, 0.f};  // Weighted rotations.
+
+  for (const auto& layer : layers) {
+    const float weight = layer.weight;
+    if (weight <= 0.f) {  // Negative weights are considered O.
+      continue;
+    }
+    acc_weight += weight;
+
+    // Decomposes translation into a normalized vector and a length, to limit
+    // lerp error on interpolated vector length.
+    const float len2 = LengthSqr(layer.transform->translation);
+    if (len2 > 0.f) {
+      const float len = std::sqrt(len2);
+      dl = dl + len * weight;
+      dt = dt + layer.transform->translation * (weight / len);
+    }
+
+    // Accumulates weighted rotation (NLerp). Makes sure quaternions are on the
+    // same hemisphere to lerp the shortest arc (using -Q if needed)
+    const float signed_weight =
+        std::copysign(weight, Dot(dr, layer.transform->rotation));
+    dr = dr + layer.transform->rotation * signed_weight;
+  }
+
+  // Copy to output
+
+  // Normalizes translation.
+  // Uses square of acc_weight as weight was multiplied twice (to dt and dl).
+  const float norm = acc_weight == 0.f ? 0.f : dl / (acc_weight * acc_weight);
+  output->translation = dt * norm;
+
+  // Normalizes rotation, doesn't need acc_weight.
+  output->rotation = NormalizeSafe(dr, ozz::math::Quaternion::identity());
+
+  // No scale.
+  output->scale = {1.f, 1.f, 1.f};
+
+  return true;
+}
+}  // namespace animation
+}  // namespace ozz
